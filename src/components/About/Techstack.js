5d86610f9ed6f79ecf6109e110971ea10f73d001
Chibuike Obiora <104082031+Obiorachibuike@users.noreply.github.com>
2025-06-09 22:10:58 +0100

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,61 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
+  DiJavascript1, DiReact, DiNodejs, DiMongodb, DiPython, DiGit, DiJava, DiHtml5,
 } from "react-icons/di";
 import {
-  SiNextdotjs,
-  SiMysql,
-  SiTypescript,
-  SiHtml5,
+  SiNextdotjs, SiMysql, SiTypescript, SiSolidity, SiEthereum, SiTailwindcss,
+  SiRedux, SiExpress, SiWeb3Dotjs, SiEthers, SiHardhat, SiTruffle,
 } from "react-icons/si";
+import { CgCPlusPlus } from "react-icons/cg";
+import "./Techstack.css"; // 👈 create this file for animations
 
 function Techstack() {
+  const ref = useRef();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setIsVisible(entry.isIntersecting),
+      { threshold: 0.3 }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, []);
+
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHtml5 />
+    <Row
+      ref={ref}
+      className={`techstack-row ${isVisible ? "visible" : ""}`}
+      style={{ justifyContent: "center", paddingBottom: "50px" }}
+    >
+      {[
+        <DiJavascript1 />, <SiTypescript />, <DiNodejs />, <SiExpress />,
+        <DiReact />, <SiNextdotjs />, <SiRedux />, <SiTailwindcss />,
+        <DiMongodb />, <SiMysql />, <DiGit />, <DiHtml5 />, <CgCPlusPlus />,
+        <DiPython />, <DiJava />, <SiSolidity />, <SiEthereum />, <SiEthers />,
+        <SiWeb3Dotjs />, <SiHardhat />, <SiTruffle />,
+      ].map((Icon, idx) => (
+        <Col key={idx} xs={4} md={2} className="tech-icons">
+          {Icon}
         </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTypescript />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
+      ))}
     </Row>
   );
 }
