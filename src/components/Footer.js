d1913e9d37002ce06f6f3f09173e440407f26174
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-06-24 11:39:50 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
           <h3>Designed and Developed by UDAY</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} KodexTech</h3>
+          <h3>Copyright © {year} KodexTeck</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
