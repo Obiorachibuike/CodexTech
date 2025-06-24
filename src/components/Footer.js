dc7acc2d006ee3f56f200e786fced5f65266d17a
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-06-24 11:39:05 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/oxBinaryBrain"
+                href="https://github.com/obiorachibuike"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
