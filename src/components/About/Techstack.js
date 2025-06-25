070fa5be3665bc5d04da194cbed0ff56a582c50d
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-06-26 00:54:18 +0100

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
@@ -9,53 +8,112 @@ import {
   DiPython,
   DiGit,
   DiJava,
+  DiSass,
+  DiHtml5,
+  DiBootstrap,
 } from "react-icons/di";
 import {
   SiNextdotjs,
-  SiMysql,
   SiTypescript,
-  SiHtml5,
+  SiMysql,
+  SiRedux,
+  SiDocker,
+  SiTailwindcss,
+  SiGraphql,
+  SiPostgresql,
+  SiFirebase,
+  SiAmazonaws,
+  SiVuedotjs,
+  SiExpress,
+  SiNestjs,
+  SiSolidity,
+  SiWeb3Dotjs,
+  SiHardhat,
+  SiTruffle,
+  SiIpfs,
+  SiChainlink,
+  SiEthers,
+  SiRedis,
+  SiVercel,
 } from "react-icons/si";
+import { CgCPlusPlus } from "react-icons/cg";
+import "./Techstack.css";
+
+const icons = [
+  <DiHtml5 />,
+  <DiSass />,
+  <DiJavascript1 />,
+  <SiTypescript />,
+  <DiReact />,
+  <SiNextdotjs />,
+  <SiVuedotjs />,
+  <SiTailwindcss />,
+  <DiBootstrap />,
+  <DiGit />,
+  <DiNodejs />,
+  <SiExpress />,
+  <SiNestjs />,
+  <DiMongodb />,
+  <SiMysql />,
+  <SiPostgresql />,
+  <SiRedis />,
+  <SiFirebase />,
+  <DiPython />,
+  <DiJava />,
+  <CgCPlusPlus />,
+  <SiGraphql />,
+  <SiDocker />,
+  <SiAmazonaws />,
+  <SiVercel />,
+  <SiSolidity />,
+  <SiHardhat />,
+  <SiTruffle />,
+  <SiWeb3Dotjs />,
+  <SiEthers />,
+  <SiIpfs />,
+  <SiChainlink />,
+];
 
 function Techstack() {
+  const containerRef = useRef(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+          observer.unobserve(containerRef.current!);
+        }
+      },
+      { threshold: 0.2 }
+    );
+
+    if (containerRef.current) observer.observe(containerRef.current);
+
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current);
+    };
+  }, []);
+
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHtml5 />
+    <Row
+      ref={containerRef}
+      style={{ justifyContent: "center", paddingBottom: "50px" }}
+    >
+      {icons.map((IconComponent, index) => (
+        <Col
+          xs={4}
+          md={2}
+          key={index}
+          className={`tech-icons fade-up ${visible ? "visible" : ""}`}
+          style={{
+            animationDelay: `${index * 100}ms`,
+          }}
+        >
+          {IconComponent}
         </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTypescript />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
+      ))}
     </Row>
   );
 }
