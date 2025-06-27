b249b418beb18a5688dfa77b92534664eadae489
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-06-27 07:45:51 +0100

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,58 +3,71 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-import LMmodel from "../../Assets/Projects/LMmodel.png"; // Default fallback image
+import LMmodel from "../../Assets/Projects/LMmodel.png";
 
-function ProjectCards({ imgPath, title, description, ghLink, demoLink, techIcons = [] }) {
+function ProjectCards({
+  imgPath,
+  title,
+  description,
+  ghLink,
+  demoLink,
+  techIcons = [],
+  visible,
+  fadingOut,
+}) {
   return (
-    <Card className="project-card-view">
-      <Card.Img
-        variant="top"
-        src={imgPath || LMmodel}
-        alt="project-preview"
-        onError={(e) => {
-          e.target.onerror = null;
-          e.target.src = LMmodel; // fallback in case of broken link
-        }}
-      />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>{description}</Card.Text>
-
-        {/* Tech Stack Icons */}
-        {techIcons.length > 0 && (
-          <div className="tech-icons-wrapper" style={{ margin: "10px 0" }}>
-            {techIcons.map((tech, index) => {
-              const IconComponent = tech.icon;
-              return (
-                <span key={index} title={tech.name} style={{ fontSize: "1.5rem", marginRight: "8px" }}>
-                  <IconComponent />
-                </span>
-              );
-            })}
+    <div
+      className={`project-card fade-in-up ${
+        visible ? "visible" : ""
+      } ${fadingOut ? "fading-out" : ""}`}
+    >
+      <Card className="project-card-view">
+        <Card.Img
+          variant="top"
+          src={imgPath || LMmodel}
+          alt="project-preview"
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = LMmodel;
+          }}
+        />
+        <Card.Body className="d-flex flex-column justify-content-between">
+          <div>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text style={{ textAlign: "justify" }}>{description}</Card.Text>
+            {techIcons.length > 0 && (
+              <div className="tech-icon-row">
+                {techIcons.map((tech, index) => {
+                  const IconComponent = tech.icon;
+                  return (
+                    <span key={index} title={tech.name} className="tech-icon">
+                      <IconComponent />
+                    </span>
+                  );
+                })}
+              </div>
+            )}
           </div>
-        )}
-
-        {/* GitHub Button */}
-        {ghLink && (
-          <Button variant="primary" href={ghLink} target="_blank">
-            <BsGithub /> &nbsp;GitHub
-          </Button>
-        )}
-
-        {/* Demo Button - optional */}
-        {demoLink && (
-          <Button
-            variant="primary"
-            href={demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;Live
-          </Button>
-        )}
-      </Card.Body>
-    </Card>
+          <div className="d-flex mt-3">
+            {ghLink && (
+              <Button variant="primary" href={ghLink} target="_blank">
+                <BsGithub /> &nbsp;GitHub
+              </Button>
+            )}
+            {demoLink && (
+              <Button
+                variant="primary"
+                href={demoLink}
+                target="_blank"
+                className="ms-2"
+              >
+                <CgWebsite /> &nbsp;Live
+              </Button>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
